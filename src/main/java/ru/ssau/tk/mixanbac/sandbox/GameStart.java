1ddcfa8c1b1e74e337a8612e14009fe582a158d9
1 <n>
2020-09-12 17:06:12 +0400

    Создан класс GameStart, создано окошечко для фрейма и его можно успешно закрыть

---
+++ src/main/java/ru/ssau/tk/mixanbac/sandbox/GameStart.java
@@ -0,0 +1,14 @@
+package ru.ssau.tk.mixanbac.sandbox;
+
+import javax.swing.JFrame;
+
+public class GameStart {
+
+    public static void main(String[] args) {
+
+        JFrame startFrame = new JFrame("BubbleShooter");
+        startFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//Установим константы по определению при нажатии крестика, для закрытия приложения
+
+    }
+
+}
