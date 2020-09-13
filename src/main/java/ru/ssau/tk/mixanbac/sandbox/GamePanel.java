ce98374f01d05dfabf99d3cc46ec25125f801ffe
1 <n>
2020-09-13 18:41:30 +0400

    Задан размер в GamePanel

--- src/main/java/ru/ssau/tk/mixanbac/sandbox/GamePanel.java
+++ src/main/java/ru/ssau/tk/mixanbac/sandbox/GamePanel.java
@@ -1,7 +1,21 @@
 package ru.ssau.tk.mixanbac.sandbox;
 
 import javax.swing.JPanel;
+import java.awt.*;
 
 public class GamePanel extends JPanel {
 
+    //Field
+    public static int WIDTH = 400;
+    public static int HEIGHT = 400;
+
+    // Constructor
+    public GamePanel(){
+        super();//Вызываем конструктор JPanel
+
+        setPreferredSize(new Dimension(WIDTH, HEIGHT));//Размер окна с заданными параметрами
+        setFocusable(true);//Сделать окно активным
+        requestFocus();//Позволяет установить фокус на нужном компоненте
+
+    }
 }
