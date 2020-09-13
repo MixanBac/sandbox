1694fbb61d246949e6472f1c5384638d5ea0c98d
1 <n>
2020-09-13 14:34:45 +0400

    Изменен класс GameStart,окно видимое и по центру

--- src/main/java/ru/ssau/tk/mixanbac/sandbox/GameStart.java
+++ src/main/java/ru/ssau/tk/mixanbac/sandbox/GameStart.java
@@ -10,6 +10,10 @@ public class GameStart {
         startFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//Установим константы по определению при нажатии крестика, для закрытия приложения
 
         startFrame.setContentPane(new GamePanel());
+        startFrame.pack();//Фрейм по размерам будет занимать столько, сколько и его компоненты
+        startFrame.setLocationRelativeTo(null);//Позиция по центру
+        startFrame.setVisible(true);//Окно видимое
+
     }
 
 }
