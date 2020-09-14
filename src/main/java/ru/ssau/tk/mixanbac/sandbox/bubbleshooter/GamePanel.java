4156c0319b8ad1e89e850110f07cf65ba5ecdb85
1 <n>
2020-09-14 15:20:33 +0400

    Добавлен поток Thread

--- src/main/java/ru/ssau/tk/mixanbac/sandbox/bubbleshooter/GamePanel.java
+++ src/main/java/ru/ssau/tk/mixanbac/sandbox/bubbleshooter/GamePanel.java
@@ -3,11 +3,13 @@ package ru.ssau.tk.mixanbac.sandbox.bubbleshooter;
 import javax.swing.JPanel;
 import java.awt.*;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable {//Используем интерфэйс для создания потока
 
     //Field
     public static int WIDTH = 400;
     public static int HEIGHT = 400;
+
+    private Thread thread = new Thread(this);//Создание потока
 
     // Constructor
     public GamePanel(){
@@ -16,6 +18,6 @@ public class GamePanel extends JPanel {
         setPreferredSize(new Dimension(WIDTH, HEIGHT));//Размер окна с заданными параметрами
         setFocusable(true);//Сделать окно активным
         requestFocus();//Позволяет установить фокус на нужном компоненте
-
+        thread.start();//Запускаем поток
     }
 }
