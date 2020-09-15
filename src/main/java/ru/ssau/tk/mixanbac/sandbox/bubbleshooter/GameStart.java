8f89a61bda05bb67f5c3436cddcf35ce481b935a
1 <n>
2020-09-15 04:30:08 +0400

    BubbleShooter. Запущен поток

--- src/main/java/ru/ssau/tk/mixanbac/sandbox/bubbleshooter/GameStart.java
+++ src/main/java/ru/ssau/tk/mixanbac/sandbox/bubbleshooter/GameStart.java
@@ -5,14 +5,15 @@ import javax.swing.JFrame;
 public class GameStart {
 
     public static void main(String[] args) {
-
+        GamePanel panel = new GamePanel();//Создали панель
         JFrame startFrame = new JFrame("BubbleShooter");
         startFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//Установим константы по определению при нажатии крестика, для закрытия приложения
 
-        startFrame.setContentPane(new GamePanel());
+        startFrame.setContentPane(panel);
         startFrame.pack();//Фрейм по размерам будет занимать столько, сколько и его компоненты
         startFrame.setLocationRelativeTo(null);//Позиция по центру
         startFrame.setVisible(true);//Окно видимое
+        panel.start();//Запуск потока
 
     }
 
