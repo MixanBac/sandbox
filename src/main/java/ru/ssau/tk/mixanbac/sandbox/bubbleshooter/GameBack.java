9ea2bf893d85282cf5428ecffba9c1d0265b7e9c
1 <n>
2020-09-15 04:10:49 +0400

    BubbleShooter. Создание класса GameBack

---
+++ src/main/java/ru/ssau/tk/mixanbac/sandbox/bubbleshooter/GameBack.java
@@ -0,0 +1,23 @@
+package ru.ssau.tk.mixanbac.sandbox.bubbleshooter;
+
+import java.awt.*;
+
+public class GameBack {
+    //Fields
+    private Color color;
+
+    //Constructor
+    public GameBack(){
+        color = Color.BLUE;//Задание цвета
+    }
+
+    //Functions
+    public void update(){
+
+    }
+
+    public void draw(Graphics2D g){//Передача кисточки
+        g.setColor(color);//Придача кисточке цвета
+        g.fillRect(0, 0, GamePanel.WIDTH, GamePanel.HEIGHT);//Создали прямоугольник игры
+    }
+}
