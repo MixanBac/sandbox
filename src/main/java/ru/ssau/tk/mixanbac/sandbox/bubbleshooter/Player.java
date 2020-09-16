4fb10be9b8e77ca398a123bf0bc63ffa0ef1c439
1 <n>
2020-09-16 12:25:35 +0400

    BubbleShooter. Создание класса Player, задание начальных позиций(конструктор Player) и его отрисовка(метод draw)

---
+++ src/main/java/ru/ssau/tk/mixanbac/sandbox/bubbleshooter/Player.java
@@ -0,0 +1,35 @@
+package ru.ssau.tk.mixanbac.sandbox.bubbleshooter;
+
+import java.awt.*;
+
+public class Player {
+
+    //Fields
+    private double x;
+    private double y;
+    private int r;
+
+    private  Color color1;
+    private  Color color2;
+
+    //Constructor
+    public Player(){//Инициализация игрока
+        x = GamePanel.WIDTH/2;//Начальная позиция игрока
+        y = GamePanel.HEIGHT/2;
+
+        r = 5;
+
+        color1 = Color.WHITE;
+
+    }
+
+    //Functions
+    public void update(){//Обновление данных об игроке
+
+    }
+
+    public void draw(Graphics2D g){//Для рисования
+        g.setColor(color1);//Рисование 1 цветом
+        g.fillOval((int) (x - r),(int) (y - r), 2 * r, 2 * r);//Смещение потому, что java рисует от верхнего левого угла
+    }
+}
