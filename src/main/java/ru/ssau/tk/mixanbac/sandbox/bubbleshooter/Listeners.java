8329d8ec0085c7ef4c12b0874fdb91b39b890770
1 <n>
2020-09-17 22:28:42 +0400

    BubbleShooter. Добавлено считывание с клавиш числового кода(keyPressed и keyReleased)

--- src/main/java/ru/ssau/tk/mixanbac/sandbox/bubbleshooter/Listeners.java
+++ src/main/java/ru/ssau/tk/mixanbac/sandbox/bubbleshooter/Listeners.java
@@ -6,11 +6,43 @@ import java.awt.event.KeyListener;
 public class Listeners implements KeyListener {//Для управления кнопками
 
     public  void  keyPressed(KeyEvent e){//Определяет, была ли нажата клавиша на клавиатуре
+        int key = e.getKeyCode();//Числовой код нажатой на клавиатуре клавишей
 
+        if(key == KeyEvent.VK_W){//Сравниваем с нажатием на кнопку W
+            Player.up = true;
+        }
+
+        if(key == KeyEvent.VK_S){//Сравниваем с нажатием на кнопку S
+            Player.down = true;
+        }
+
+        if(key == KeyEvent.VK_A){//Сравниваем с нажатием на кнопку A
+            Player.left = true;
+        }
+
+        if(key == KeyEvent.VK_D){//Сравниваем с нажатием на кнопку D
+            Player.right = true;
+        }
     }
 
     public  void  keyReleased(KeyEvent e){//Вызывается при отпускании любой клавиши на клавиатуре
+        int key = e.getKeyCode();//Числовой код нажатой на клавиатуре клавишей
 
+        if(key == KeyEvent.VK_W){//Сравниваем с нажатием на кнопку W
+            Player.up = false;
+        }
+
+        if(key == KeyEvent.VK_S){//Сравниваем с нажатием на кнопку S
+            Player.down = false;
+        }
+
+        if(key == KeyEvent.VK_A){//Сравниваем с нажатием на кнопку A
+            Player.left = false;
+        }
+
+        if(key == KeyEvent.VK_D){//Сравниваем с нажатием на кнопку D
+            Player.right = false;
+        }
     }
 
     public void keyTyped(KeyEvent e){//Вызывается системой каждый раз, когда пользователь нажимает на клавиатуре клавиши символы Unicode
