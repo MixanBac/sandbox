64ddb166f34c42c71f1613b81c0e1ae78a2251a0
1 <n>
2020-09-17 22:05:46 +0400

    BubbleShooter. Создан класс Listeners для обработки данных с клавиатуры

---
+++ src/main/java/ru/ssau/tk/mixanbac/sandbox/bubbleshooter/Listeners.java
@@ -0,0 +1,20 @@
+package ru.ssau.tk.mixanbac.sandbox.bubbleshooter;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Listeners implements KeyListener {//Для управления кнопками
+
+    public  void  keyPressed(KeyEvent e){//Определяет, была ли нажата клавиша на клавиатуре
+
+    }
+
+    public  void  keyReleased(KeyEvent e){//Вызывается при отпускании любой клавиши на клавиатуре
+
+    }
+
+    public void keyTyped(KeyEvent e){//Вызывается системой каждый раз, когда пользователь нажимает на клавиатуре клавиши символы Unicode
+
+    }
+
+}
