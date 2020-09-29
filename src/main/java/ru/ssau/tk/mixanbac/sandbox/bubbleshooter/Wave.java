6f5839c16c7b7cf7f3d6e8a54861341e464ef0b0
1 <n>
2020-09-29 10:49:37 +0400

    BubbleShooter. Надпись по середине экрана

--- src/main/java/ru/ssau/tk/mixanbac/sandbox/bubbleshooter/Wave.java
+++ src/main/java/ru/ssau/tk/mixanbac/sandbox/bubbleshooter/Wave.java
@@ -4,72 +4,79 @@ import java.awt.*;
 
 public class Wave {
 
-    //Fields
-    private int waveNumber;
-    private int waveMultiplier;
+    private int waveNumber;//Номер волны
+    private int waveMultiplier;//Кол врагов в волне
     private long waveTimer;//Таймер
-    private long waveDelay;
-    private long waveTimerDiff;
+    private long waveDelay;//Время между волнами
+    private long waveTimerDiff;//Разница врем
+    private String waveText;//Сообщение волны
 
-    private String waveText;
 
-    //Constructor
-    public Wave() {//Инициализация переменных
+    // Constructor
+
+    public Wave() {
         waveNumber = 1;
-        waveMultiplier = 5;//Количество врагов в первую волну
+        waveMultiplier = 5;
         waveTimer = 0;
-        waveDelay = 5000;//Количество миллисекунд
+        waveDelay = 5000;
         waveTimerDiff = 0;
-
-        waveText = " В О Л Н А -";
-
+        waveText = "ВОЛНА";
     }
-    //Functions
 
-    public void createEnemies() {//Создание врагов
-        int enemyCount = waveNumber * waveMultiplier;
-        if (waveNumber < 4) {
-            while (enemyCount > 0) {
-                int type = 1;
-                int rank = 1;
-                GamePanel.enemies.add(new Enemy(type, rank));//Создание врагов
-                enemyCount -= type * rank;//Вычитание стоимости врага из инвестиций
+    //Создание врагов
+    public void createEnemies() {
+        int enemyCount = waveNumber * waveMultiplier;//Количество врагов
+        if (waveNumber < 10) {
+            while (enemyCount > 0) {//Пока
+                int type = 1;//
+                int rank = 1;//
+                GamePanel.enemies.add(new Enemy(type, rank));//
+                enemyCount -= type * rank;//Конец цикла создания врагов
             }
-
         }
+        waveNumber++;
     }
 
-    public void update() {//После каждой волны ждет 5 минут и запустит новую волну
-        if (GamePanel.enemies.size() == 0 && waveTimer == 0) ;
-        {//Если живых врагов не осталось
-            waveTimer = System.nanoTime();
+    //
+    public void update() {
+        //Обновление таймера
+        if (GamePanel.enemies.size() == 0 && waveTimer == 0) {//Если нет врагов и таймер=0
+            waveTimer = System.nanoTime();//= текущее время
+        }
+        //Проверка на создание врагов
+        if (waveTimer > 0) {//Если таймер больше времени задержки
+            waveTimerDiff += (System.nanoTime() - waveTimer) / 1000000;//Разница времени
+            waveTimer = System.nanoTime();//Время
 
         }
-        if (waveTimer > 0) {
-            waveTimerDiff += (System.nanoTime() - waveTimer) / 1000000;
-            waveTimer = System.nanoTime();
-        }
-        if (waveTimerDiff > waveDelay) {
-            createEnemies();//Создание врагов
-            waveTimer = 0;
+        if (waveTimerDiff > waveDelay) {//Если пауза закончилась
+            createEnemies(); //Создаём врагов
+            waveTimer = 0;//Таймер
             waveTimerDiff = 0;
-        }
 
+        }
     }
 
-    public boolean showWave() {//Будет ли показываться надпись или нет
-        if (waveTimer != 0) {//Показываем пока не обнулился таймер
+    //Показ надписи
+    public boolean showWave() {
+        if (waveTimer != 0) {//Если таймер не равен нулю
             return true;
         } else return false;
     }
 
-    public void draw(Graphics2D g) {//Создание прозрачности надписи
-        double divider = waveDelay / 180;
-        double alpha = waveTimerDiff / divider;
-        alpha = 255 * Math.sin(Math.toRadians(alpha));//Нахождение синуса от числа переведенного в радианы
-        g.setColor(new Color(255, 255, 255, (int) alpha));
-        g.drawString(waveText, GamePanel.WIDTH / 2, GamePanel.HEIGHT / 2);//Рисование строки посередине
+    //Отрисовка
+    public void draw(Graphics2D g) {
+        //Закон изменения прозрачности
+        double divider = waveDelay / 180; //Сколько вемени прийдется на 1 градус изменения цвеа
+        double alpha = waveTimerDiff / divider;//Показатель прозрачности в опреденный момент паузы
+        alpha = 255 * Math.sin(Math.toRadians(alpha)); //Переводим прозрачн в радианы и определ синус и умнож на уровень прозррачности
+        if (alpha < 0) alpha = 0;
+        if (alpha > 255) alpha = 255;
+        g.setFont(new Font("consolas", Font.PLAIN, 20));//Передаём шрифт
+        g.setColor(new Color(255, 255, 255, (int) alpha));//Полная строка
+        String s = waveNumber + "ая " + waveText; //Полная строка
+        long length = (int) g.getFontMetrics().getStringBounds(s, g).getWidth();//Длина надписи в пиксилях
+        g.drawString(s, GamePanel.WIDTH / 2 - (int) (length / 2), GamePanel.HEIGHT / 2);//Рисуем строчку в центре панели
 
     }
-
 }
