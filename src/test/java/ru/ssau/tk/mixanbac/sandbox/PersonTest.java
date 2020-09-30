c92820c86179199998521f70d76229fc93133624
1 <n>
2020-09-30 13:06:28 +0400

    1.11 Написано три отдельных тестовых метода для тестирования соответствующих методов каждого поля

--- src/test/java/ru/ssau/tk/mixanbac/sandbox/PersonTest.java
+++ src/test/java/ru/ssau/tk/mixanbac/sandbox/PersonTest.java
@@ -5,24 +5,68 @@ import org.testng.annotations.Test;
 import static org.testng.Assert.*;
 
 public class PersonTest {
-
-    private final static double DELTA = 0.00001;
+    private final Person firstPerson = new Person();
+    private final Person secondPerson = new Person();
+    private final Person thirdPerson = new Person();
+    @Test
+    public void testFirstName() {
+        assertNull(firstPerson.getFirstName(), null);
+        assertNotEquals(firstPerson.getFirstName(), "");
+        firstPerson.setFirstName("");
+        assertEquals(firstPerson.getFirstName(), "");
+        firstPerson.setFirstName("Mixan");
+        assertEquals(firstPerson.getFirstName(), "Mixan");
+    }
+    @Test
+    public void testLastName() {
+        assertNull(secondPerson.getLastName(), null);
+        assertNotEquals(secondPerson.getLastName(), "");
+        secondPerson.setLastName("");
+        assertEquals(secondPerson.getLastName(), "");
+        secondPerson.setLastName("Elena");
+        assertNotEquals(secondPerson.getLastName(), "Ivanova");
+    }
 
     @Test
-    public void testMethods() {
+    public void testPassportID() {
+        assertEquals(thirdPerson.getPassportId(), 0);
+        thirdPerson.setPassportId(953023);
+        assertEquals(thirdPerson.getPassportId(),953023);
+        assertNotEquals(thirdPerson.getPassportId(),859423);
+    }
+
+    @Test
+    public void testNameConstructor() {
         Person firstPerson = new Person();
-        Person secondPerson = new Person();
-        firstPerson.setFirstName("Mixan");
-        secondPerson.setFirstName("Bac");
-        firstPerson.setLastName("Lena");
-        secondPerson.setLastName("Sokol");
-        firstPerson.setPassportId(774847584);
-        secondPerson.setPassportId(734873475);
-        assertEquals(firstPerson.getFirstName(), "Mixan");
-        assertEquals(secondPerson.getFirstName(), "Bac");
-        assertEquals(firstPerson.getLastName(), "Lena");
-        assertEquals(secondPerson.getLastName(), "Sokol");
-        assertEquals(firstPerson.getPassportId(), 774847584, DELTA);
-        assertEquals(secondPerson.getPassportId(), 734873475, DELTA);
+        Person secondPerson = new Person(null, null);
+
+        assertEquals(firstPerson.getFirstName(), null);
+        assertEquals(firstPerson.getLastName(), null);
+        assertNull(secondPerson.getFirstName());
+        assertNull(secondPerson.getLastName());
+    }
+
+    @Test
+    public void testPassportIdConstructor () {
+        Person person = new Person(555);
+        assertEquals(person.getPassportId(), 555);
+        assertNotEquals(person.getPassportId(), 777);
+    }
+
+    @Test
+    public void testFullConstructor () {
+        Person firstPerson = new Person("Vlada", "Petrova", 953023);
+        Person secondPerson = new Person(null, null, 839393);
+
+        assertEquals(firstPerson.getFirstName(), "Vlada");
+        assertEquals(firstPerson.getLastName(), "Petrova");
+        assertEquals(firstPerson.getPassportId(), 953023);
+        assertNull(secondPerson.getFirstName());
+        assertNull(secondPerson.getLastName());
     }
 }
+
+
+
+
+
