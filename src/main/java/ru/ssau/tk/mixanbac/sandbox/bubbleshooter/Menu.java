52d210b9e23bfe42bd479a10708c5d7397e37420
1 <n>
2020-10-02 21:27:01 +0400

    BubbleShooter. Написан текст в меню на кнопке во время игры

--- src/main/java/ru/ssau/tk/mixanbac/sandbox/bubbleshooter/Menu.java
+++ src/main/java/ru/ssau/tk/mixanbac/sandbox/bubbleshooter/Menu.java
@@ -5,16 +5,21 @@ import java.awt.*;
 public class Menu {
 
     //Fields
+
     private int buttonWidth;
     private int buttonHeight;
     private Color buttonColor;
+    private String s;
 
     //Constructor
+
     public Menu(){
-        buttonWidth = 120;
+        buttonWidth = 260;
         buttonHeight = 60;
         buttonColor = Color.white;
+        s = "Начать игру";
     }
+
     //Functions
 
     public void draw(Graphics2D g){
@@ -23,6 +28,10 @@ public class Menu {
         g.drawRect(GamePanel.WIDTH/2 - buttonWidth/2,
                 GamePanel.HEIGHT/2 - buttonHeight/2, buttonWidth, buttonHeight);//Рисуем прямоугольник
         g.setStroke(new BasicStroke(1));//Вернем толщину линии в исходное значение после рисования кнопки
+        g.setColor(buttonColor);//Цвет текста на кнопке
+        g.setFont(new Font("Consoles", Font.BOLD, 40));//Шрифт кнопки
+        long length = (int) g.getFontMetrics().getStringBounds(s, g).getWidth();//Длина строки в кнопке
+        g.drawString(s, (int) (GamePanel.WIDTH/2 - length/2),(int) (GamePanel.HEIGHT/2 + buttonHeight/4));
 
     }
 }
