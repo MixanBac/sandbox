e03a5507d7413c489e5c498d8525c465238d34be
1 <n>
2020-10-09 23:05:14 +0400

    BubbleShooter. Рисование строки в верхней части экрана

--- src/main/java/ru/ssau/tk/mixanbac/sandbox/bubbleshooter/GameBack.java
+++ src/main/java/ru/ssau/tk/mixanbac/sandbox/bubbleshooter/GameBack.java
@@ -18,6 +18,16 @@ public class GameBack {
 
     public void draw(Graphics2D g) {//Передача кисточки
         g.setColor(color);//Придача кисточке цвета
-        g.fillRect(0, 0, GamePanel.WIDTH, GamePanel.HEIGHT);//Создали прямоугольник игры
+        g.fillRect(0, 0, GamePanel.WIDTH, GamePanel.HEIGHT);//Создание прямоугольную область
+
+        Color colorString = new Color(200, 200, 0);//Создание объекта класса цвет
+        g.setColor(colorString);//Придаем цвет объекту
+        g.fillRect(0, 0, GamePanel.WIDTH, 20);//Рисуем прямоугольную область
+        g.setColor(Color.WHITE);//Новый цвет
+        Font font = new Font("Arial", Font.ITALIC, 20);//Создание объекта типа Font
+        g.setFont(font);//Установка шрифта
+
+        ((Graphics2D) g).drawString("", 50, 15);//Отрисовка строки
+
     }
 }
