f56137186a951ecfa7dce0575527a1a0d3923ae7
1 <n>
2020-10-20 20:50:47 +0400

    BubbleShooter. Не буду пользоваться прицелом с картинки(Не понравился)

--- src/main/java/ru/ssau/tk/mixanbac/sandbox/bubbleshooter/GamePanel.java
+++ src/main/java/ru/ssau/tk/mixanbac/sandbox/bubbleshooter/GamePanel.java
@@ -82,16 +82,16 @@ public class GamePanel extends JPanel implements Runnable {//Используе�
         aim1 = new Aim(GamePanel.mouseX, GamePanel.mouseY, 72, 76, "res/aim1.png", 0, 0);
 
         Toolkit kit = Toolkit.getDefaultToolkit();//Курсор
-        Cursor myCursor = kit.getDefaultToolkit().createCustomCursor(kit.getDefaultToolkit().getImage(""),
-                new Point(0, 0), "myCursor");//Пустой курсор
-        /*BufferedImage buffered = new BufferedImage(16, 16, BufferedImage.TYPE_INT_ARGB);//Создание маленького "мольбертика" для рисования//Курсор
+        /*Cursor myCursor = kit.getDefaultToolkit().createCustomCursor(kit.getDefaultToolkit().getImage(""),
+                new Point(0, 0), "myCursor");//Пустой курсор*/
+        BufferedImage buffered = new BufferedImage(16, 16, BufferedImage.TYPE_INT_ARGB);//Создание маленького "мольбертика" для рисования//Курсор
         Graphics2D g3 = (Graphics2D) buffered.getGraphics();
         g3.setColor(new Color(255, 255, 255));//Цвет мишени
         g3.drawOval(0, 0, 4, 4);//Окружение миншени
         g3.drawLine(2, 0, 2, 4);
         g3.drawLine(0, 2, 4, 2);
         Cursor myCursor = kit.createCustomCursor(buffered, new Point(3, 3), "myCursor");
-        g3.dispose();*/
+        g3.dispose();
 
         while (true) { //TODO States
 
